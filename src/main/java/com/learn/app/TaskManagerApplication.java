46dbdd3c039afc6c499ee6c4ae3603514d0cd269
floreaGabriel <g.florea2004@gmail.com>
2025-10-20 21:49:45 +0300

    [EDIT] Edited to initial configuration.

--- src/main/java/com/learn/app/TaskManagerApplication.java
+++ src/main/java/com/learn/app/TaskManagerApplication.java
@@ -1,13 +1,14 @@
 package com.learn.app;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class TaskManagerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TaskManagerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(TaskManagerApplication.class, args);
+  }
 
 }
